173ac86fc20ceb3c52635220a4e11db0f813a156
corphish <d97.avinaba@gmail.com>
2021-02-26 00:27:09 +0530

    styles: theme: Update colors
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/puppyadoption/ui/theme/Color.kt
+++ app/src/main/java/com/corphish/puppyadoption/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.corphish.puppyadoption.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Orange200 = Color(0xffff8533)
+val Orange500 = Color(0xffff6600)
+val Orange700 = Color(0xffcc5200)
 val Teal200 = Color(0xFF03DAC5)
