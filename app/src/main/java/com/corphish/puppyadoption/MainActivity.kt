83fa6591316ab87ad319ec7ec1174f53fd68144c
corphish <d97.avinaba@gmail.com>
2021-02-25 18:51:05 +0530

    MainActivity: Add top app bar
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/puppyadoption/MainActivity.kt
+++ app/src/main/java/com/corphish/puppyadoption/MainActivity.kt
@@ -3,11 +3,14 @@ package com.corphish.puppyadoption
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.corphish.puppyadoption.ui.theme.PuppyAdoptionTheme
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +32,27 @@ class MainActivity : AppCompatActivity() {
  */
 @Composable
 fun MainLayout() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                    )
+                },
+                backgroundColor = MaterialTheme.colors.background,
+                elevation = 2.dp
+            )
+        },
+        content = { MainContent() }
+    )
+}
+
+/**
+ * Main content of the app.
+ */
+@Composable
+fun MainContent() {
     Greeting("Android")
 }
 
