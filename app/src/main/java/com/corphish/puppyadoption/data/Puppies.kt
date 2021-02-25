59e3c5ae8175139b1b22c32b3e8537dfb03c9bde
corphish <d97.avinaba@gmail.com>
2021-02-25 21:36:15 +0530

    Puppies: Add more puppies
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/puppyadoption/data/Puppies.kt
+++ app/src/main/java/com/corphish/puppyadoption/data/Puppies.kt
@@ -9,6 +9,7 @@ object Puppies {
 
     /**
      * Static list of all the puppies.
+     * P.S - It will take a while for me to describe all the puppies.
      */
     val LIST = listOf(
         Puppy(
@@ -17,14 +18,38 @@ object Puppies {
             breed = "Beagle",
             age = "6 months",
             imageURL = "https://i.pinimg.com/originals/f7/2e/31/f72e31a9a382678f511ba44318da8237.png",
-            description = "Beagles are cute.",
+            description = "Hi hooman! I am sure that you have already fallen in love with me coz I am soo cute. I love hoomans too! I will love running and sniffing around your house \uD83D\uDE1C. P.S - Gib me loads of toys to chew \uD83D\uDE43.",
         ),
         Puppy(
             id = 1,
             name = "Jimmy",
             breed = "Samoyed",
-            age = "1 year",
-            imageURL = "https://thumbs.dreamstime.com/b/face-happy-samoyed-dog-36472428.jpg",
+            age = "8 months",
+            imageURL = "https://worlddogfinder.com/upload/media/samoyed_smiling_muF1U2NDQ6.jpg",
+            description = "",
+        ),
+        Puppy(
+            id = 2,
+            name = "Goldy",
+            breed = "Golden Retriever",
+            age = "1.1 years",
+            imageURL = "https://thehappypuppysite.com/wp-content/uploads/2017/12/puppy9.jpg",
+            description = "",
+        ),
+        Puppy(
+            id = 3,
+            name = "Leonardo",
+            breed = "Husky",
+            age = "5 months",
+            imageURL = "https://st.depositphotos.com/2588973/2940/i/950/depositphotos_29401593-stock-photo-close-up-of-husky-puppy.jpg",
+            description = "",
+        ),
+        Puppy(
+            id = 4,
+            name = "Maya",
+            breed = "Husky",
+            age = "3 months",
+            imageURL = "https://theawesomedaily.com/wp-content/uploads/2017/04/corgi-puppies-3a-1.jpg",
             description = "",
         ),
     )
